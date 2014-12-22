87dc41dc08f19cedd705c58786ea5d736b40e95b
Haifeng Li <omycle@gmail.com>
2014-12-22 10:10:56 +0800

    Retrived from /proc/kpagecount
    Signed-off-by: Haifeng Li <omycle@gmail.com>

--- memory-plumbling.c
+++ memory-plumbling.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> // for pread
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <assert.h>
-// for pread
+#include <inttypes.h> // for uint64_t
 
-typedef unsigned long uint64_t;
-uint64_t buf[0x10000];
+#define BUF_SIZE 0x100000 //PAGE_SHIFT is 4KB,so BUF_SIZE should cover 4GB
+unsigned long long buf[BUF_SIZE]; //uint64_t is 8 bytes
 
 void main()
 {
-int offset =0;
-int size = 0x10000;
-long  bytes;
-int fd_kpagecount = open("/proc/kpagecount", O_RDONLY);
-assert(fd_kpagecount!=0);
-bytes = pread(fd_kpagecount, buf, size, offset);
-int i =0;
-printf("%lx",bytes);
-//for(i=0;i<bytes;i++);
-//printf("%lx\n",buf[i]);
+	int offset =0;
+	int  bytes;
+	int size = BUF_SIZE*8;
+	int fd_kpagecount = open("/proc/kpagecount", O_RDONLY);
+	assert(fd_kpagecount!=0);
+	bytes = pread(fd_kpagecount, buf, size, offset);
+	if(bytes == -1){
+		printf("pread error \n");
+		return;
+	}
+	printf("%s: %x\n",__func__,bytes);
+	int i=0;
+	for(;i<bytes/8;i++){
+	 if(buf[i] !=0 && buf[i]!=-127)
+	 printf("%x :  %lld \n",i,buf[i]);
+	}
 }
